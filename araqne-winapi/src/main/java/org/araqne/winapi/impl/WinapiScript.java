e7e6c54cde31ebc2d10c19ab09aa6ef580a2511f
xeraph <xeraph@nchovy.com>
2013-09-03 15:55:39 +0900

    added winapi.eventLogs command

--- araqne-winapi/src/main/java/org/araqne/winapi/impl/WinapiScript.java
+++ araqne-winapi/src/main/java/org/araqne/winapi/impl/WinapiScript.java
@@ -1,7 +1,13 @@
 package org.araqne.winapi.impl;
 
+import java.util.Iterator;
+
 import org.araqne.api.Script;
+import org.araqne.api.ScriptArgument;
 import org.araqne.api.ScriptContext;
+import org.araqne.api.ScriptUsage;
+import org.araqne.winapi.EventLog;
+import org.araqne.winapi.EventLogReader;
 import org.araqne.winapi.RegistryKey;
 
 public class WinapiScript implements Script {
@@ -27,5 +33,20 @@ public class WinapiScript implements Script {
 				key.close();
 		}
 
+	}
+
+	@ScriptUsage(description = "print recent event logs", arguments =
+			@ScriptArgument(name = "source name", type = "string", description = "event source name"))
+	public void eventLogs(String[] args) {
+		EventLogReader reader = new EventLogReader(args[0]);
+		Iterator<EventLog> it = reader.iterator();
+
+		long count = 0;
+		while (it.hasNext()) {
+			if (count >= 10)
+				break;
+			context.println(it.next());
+			count++;
+		}
 	}
 }
