0f472f126f27a38a7059dccc56034cbce9b7f0a7
xeraph <xeraph@eediom.com>
2014-09-05 14:21:09 +0900

    closed araqne/issue#526, araqne/issue#202. support osx disk usage check

--- araqne-linux-api/src/main/java/org/araqne/linux/api/DiskPartition.java
+++ araqne-linux-api/src/main/java/org/araqne/linux/api/DiskPartition.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DiskPartition {
 	private String name;
@@ -49,25 +51,85 @@ public class DiskPartition {
 		this.path = path;
 	}
 
-	public static List<DiskPartition> getDiskPartitions() {
-		List<String> lines = readPartitions();
-		return getDiskPartitions(lines);
+	@Override
+	public String toString() {
+		return "name=" + name + ", used=" + used + ", available=" + available + ", path=" + path;
 	}
 
-	public static List<DiskPartition> getDiskPartitions(List<String> lines) {
+	public static List<DiskPartition> getDiskPartitions() {
+		List<String> lines = readPartitions();
+		return parseDiskPartitions(lines);
+	}
+
+	public static List<DiskPartition> parseDiskPartitions(List<String> lines) {
 		List<DiskPartition> stats = new ArrayList<DiskPartition>();
 
-		for (int index = 0; index < lines.size(); index++) {
+		List<String> headers = parseHeader(lines.get(0));
+		int columnCount = headers.size();
+
+		for (int index = 1; index < lines.size(); index++) {
 			String line = lines.get(index);
-			String[] parts = line.split("[ ]+");
-			while (parts.length != 6) {
+			List<String> parts = parseLine(line);
+			while (parts.size() < columnCount) {
 				line += " " + lines.get(++index);
-				parts = line.split("[ ]+");
+				parts = parseLine(line);
 			}
 
-			stats.add(getDiskStat(parts));
+			DiskPartition p = parseDiskStat(headers, parts);
+			stats.add(p);
 		}
 		return stats;
+	}
+
+	public static List<String> parseLine(String line) {
+		List<String> l = new ArrayList<String>();
+
+		Matcher m = Pattern.compile("\\s\\d+\\s").matcher(line);
+		if (!m.find()) {
+			return l;
+		}
+
+		String name = line.substring(0, m.start()).trim();
+
+		String s = line.substring(m.start()).trim();
+		int pathBegin = s.lastIndexOf('%');
+		String path = null;
+		if (pathBegin > 0) {
+			path = s.substring(pathBegin + 1).trim();
+			s = s.substring(0, pathBegin).trim();
+		}
+
+		// first space containing path
+		l.add(name);
+
+		String[] tokens = s.split("[ ]+");
+		for (String t : tokens)
+			l.add(t);
+
+		// last space containing path
+		if (path != null)
+			l.add(path);
+
+		return l;
+	}
+
+	private static List<String> parseHeader(String s) {
+		String[] tokens = s.split("[ ]+");
+		List<String> headers = new ArrayList<String>();
+
+		boolean mountToken = false;
+		for (String token : tokens) {
+			if (token.equals("Mounted")) {
+				headers.add("Mounted on");
+				mountToken = true;
+			} else if (mountToken && token.equals("on")) {
+				continue;
+			} else {
+				headers.add(token);
+			}
+		}
+
+		return headers;
 	}
 
 	private static List<String> readPartitions() {
@@ -77,7 +139,6 @@ public class DiskPartition {
 		try {
 			p = Runtime.getRuntime().exec("df");
 			br = new BufferedReader(new InputStreamReader(p.getInputStream(), Charset.forName("utf-8")));
-			br.readLine();
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				lines.add(line);
@@ -95,12 +156,40 @@ public class DiskPartition {
 		return lines;
 	}
 
-	private static DiskPartition getDiskStat(String[] parts) {
+	private static DiskPartition parseDiskStat(List<String> headers, List<String> parts) {
+		int nameIndex = getHeaderIndex(headers, "Filesystem");
+		int usedIndex = getHeaderIndex(headers, "Used");
+		int availableIndex = getHeaderIndex(headers, "Available");
+		int mountedOnIndex = getHeaderIndex(headers, "Mounted on");
+
+		int unit = 1024;
+		for (String h : headers) {
+			if (h.equals("1K-blocks")) {
+				unit = 1024;
+				break;
+			} else if (h.equals("512-blocks")) {
+				unit = 512;
+				break;
+			}
+		}
+
 		DiskPartition stat = new DiskPartition();
-		stat.setName(parts[0]);
-		stat.setUsed(Long.valueOf(parts[2]) * 1024);
-		stat.setAvailable(Long.valueOf(parts[3]) * 1024);
-		stat.setPath(parts[5]);
+		stat.setName(parts.get(nameIndex));
+		stat.setUsed(Long.valueOf(parts.get(usedIndex)) * unit);
+		stat.setAvailable(Long.valueOf(parts.get(availableIndex)) * unit);
+		stat.setPath(parts.get(mountedOnIndex));
 		return stat;
+	}
+
+	private static int getHeaderIndex(List<String> headers, String name) {
+		int i = 0;
+		for (String h : headers) {
+			if (h.equals(name))
+				return i;
+
+			i++;
+		}
+
+		throw new IllegalStateException("header not found, expected '" + name + "'");
 	}
 }
--- araqne-linux-api/src/test/java/org/araqne/linux/api/DiskPartitionTest.java
+++ araqne-linux-api/src/test/java/org/araqne/linux/api/DiskPartitionTest.java
@@ -12,34 +12,72 @@ public class DiskPartitionTest {
 	@Test
 	public void getDiskPartition1() {
 		List<String> lines = new ArrayList<String>();
+		lines.add("Filesystem           1K-blocks      Used Available Use% Mounted on");
 		lines.add("/dev/sda1        9156984 5075680   3609496  59% /");
 
-		List<DiskPartition> partitions = DiskPartition.getDiskPartitions(lines);
+		List<DiskPartition> partitions = DiskPartition.parseDiskPartitions(lines);
 		assertEquals(1, partitions.size());
-
-		DiskPartition partition = partitions.get(0);
-		assertEquals("/dev/sda1", partition.getName());
-		assertEquals(3609496l * 1024, partition.getAvailable());
-		assertEquals("/", partition.getPath());
-		assertEquals(8685176l * 1024, partition.getTotal());
-		assertEquals(5075680l * 1024, partition.getUsed());
+		verify(partitions.get(0), "/dev/sda1", 5075680l * 1024, 3609496l * 1024, "/");
 	}
 
 	@Test
 	public void getDiskPartition2() {
 		List<String> lines = new ArrayList<String>();
+		lines.add("Filesystem           1K-blocks      Used Available Use% Mounted on");
 		lines.add("/dev/mapper/vg_linux-lv_root");
 		lines.add("51609340  45357008");
 		lines.add("5728100  89% /");
 
-		List<DiskPartition> partitions = DiskPartition.getDiskPartitions(lines);
+		List<DiskPartition> partitions = DiskPartition.parseDiskPartitions(lines);
 		assertEquals(1, partitions.size());
+		verify(partitions.get(0), "/dev/mapper/vg_linux-lv_root", 45357008l * 1024, 5728100l * 1024, "/");
+	}
 
-		DiskPartition partition = partitions.get(0);
-		assertEquals("/dev/mapper/vg_linux-lv_root", partition.getName());
-		assertEquals(5728100l * 1024, partition.getAvailable());
-		assertEquals("/", partition.getPath());
-		assertEquals(51085108l * 1024, partition.getTotal());
-		assertEquals(45357008l * 1024, partition.getUsed());
+	@Test
+	public void testLinuxDiskPartitions() {
+		List<String> lines = new ArrayList<String>();
+		lines.add("Filesystem           1K-blocks      Used Available Use% Mounted on");
+		lines.add("/dev/mapper/VolGroup00-LogVol00");
+		lines.add("                      16124848   3787816  11504728  25% /");
+		lines.add("/dev/xvda1              101086     21870     73997  23% /boot");
+		lines.add("tmpfs                   524288         0    524288   0% /dev/shm");
+		lines.add("/dev/xvdb1            82567188  44004080  34368940  57% /home");
+
+		List<DiskPartition> partitions = DiskPartition.parseDiskPartitions(lines);
+		assertEquals(4, partitions.size());
+		verify(partitions.get(0), "/dev/mapper/VolGroup00-LogVol00", 3787816 * 1024L, 11504728 * 1024L, "/");
+		verify(partitions.get(1), "/dev/xvda1", 21870 * 1024, 73997 * 1024, "/boot");
+		verify(partitions.get(2), "tmpfs", 0, 524288 * 1024, "/dev/shm");
+	}
+
+	@Test
+	public void testOsxDiskPartitions() {
+		List<String> lines = new ArrayList<String>();
+		lines.add("Filesystem                        512-blocks      Used Available Capacity  iused    ifree %iused  Mounted on");
+		lines.add("/dev/disk0s2                       389057912 349665128  38880784    90% 43772139  4860098   90%   /");
+		lines.add("devfs                                    379       379         0   100%      656        0  100%   /dev");
+		lines.add("/dev/disk0s4                        78123000  50762336  27360664    65%   157817 13827411    1%   /Volumes/BOOTCAMP");
+		lines.add("map -hosts                                 0         0         0   100%        0        0  100%   /net");
+		lines.add("map auto_home                              0         0         0   100%        0        0  100%   /home");
+		lines.add("localhost:/AAr1EGYFFuA61rgZQOMlgm  389057912 389057912         0   100%        0        0  100%   /Volumes/MobileBackups");
+		lines.add("/dev/disk2s2                          415680    347288     68392    84%    43409     8549   84%   /Volumes/MacUpdate 2013 Freebie Bundle");
+
+		List<DiskPartition> partitions = DiskPartition.parseDiskPartitions(lines);
+		assertEquals(7, partitions.size());
+
+		verify(partitions.get(0), "/dev/disk0s2", 349665128L * 512, 38880784L * 512, "/");
+		verify(partitions.get(1), "devfs", 379 * 512, 0, "/dev");
+		verify(partitions.get(2), "/dev/disk0s4", 50762336L * 512, 27360664L * 512, "/Volumes/BOOTCAMP");
+		verify(partitions.get(3), "map -hosts", 0, 0, "/net");
+		verify(partitions.get(4), "map auto_home", 0, 0, "/home");
+		verify(partitions.get(5), "localhost:/AAr1EGYFFuA61rgZQOMlgm", 389057912L * 512, 0, "/Volumes/MobileBackups");
+		verify(partitions.get(6), "/dev/disk2s2", 347288L * 512, 68392L * 512, "/Volumes/MacUpdate 2013 Freebie Bundle");
+	}
+
+	private void verify(DiskPartition p, String name, long used, long available, String path) {
+		assertEquals(name, p.getName());
+		assertEquals(used, p.getUsed());
+		assertEquals(available, p.getAvailable());
+		assertEquals(path, p.getPath());
 	}
 }
